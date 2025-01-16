db7eba4bae9aa12b8a02d3bbe51b58ebd51c7069
이정환 <king199777@gmail.com>
2025-01-16 10:42:50 +0900

    Update question.js

--- missions/day05/coding-quiz/quiz1/question.js
+++ missions/day05/coding-quiz/quiz1/question.js
@@ -19,23 +19,23 @@ function printMovieReview() {
 }
 
 printMovieReview({
-  title: "오펜하이머",
-  releaseYear: 2023,
-  reviewers: ["이정환", "김효빈", "김고은"],
+  title: "하얼빈",
+  releaseYear: 2024,
+  reviewers: ["박정민", "김효빈", "이정환"],
 });
 
 // 출력 결과 :
-// 제목 : 오펜하이머
+// 제목 : 하얼빈
 // 개봉 : 2023
 // 첫 번째 리뷰어 : 이정환
 
 printMovieReview({
-  title: "웡카",
+  title: "위키드",
   releaseYear: 2024,
   reviewers: [],
 });
 
 // 출력 결과 :
-// 제목 : 웡카
+// 제목 : 위키드
 // 개봉 : 2024
 // 첫 번째 리뷰어 : 리뷰어 미정
